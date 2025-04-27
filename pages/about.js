98d1e453e9b635fb0eaf94cc497886c44801d454
EclipseEmpress24 <168150906+EclipseEmpress24@users.noreply.github.com>
2025-04-28 03:43:59 +0530

    In progress
    Signed-off-by: EclipseEmpress24 <168150906+EclipseEmpress24@users.noreply.github.com>

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,14 @@
+import Navbar from '../components/navbar';
+
+export default function About() {
+  return (
+    <>
+      <Navbar />
+      <div className="p-8 text-center">
+        <h1 className="text-4xl font-bold">About Me</h1>
+        <p className="mt-4 text-xl">I'm a passionate tech enthusiast, exploring new ideas, building projects, and learning continuously.</p>
+      </div>
+    </>
+  );
+}
 
